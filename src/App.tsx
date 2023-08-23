a91b58029927da9a57d5e4dcb8fc6f7316888db3
HSGamer <huynhqtienvtag@gmail.com>
2023-08-23 16:16:09 +0700

    separated

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import {demoQuestions} from "./question/QuestionList.tsx";
+import {demoQuestions, QuestionWithAnswer} from "./question/QuestionList.tsx";
 import {Box} from "@mantine/core";
 import {QuizForm} from "./component/QuizForm.tsx";
 
 function App() {
+    const submitAnswer = (answers: QuestionWithAnswer[]) => {
+        console.log(answers)
+    };
+
     return (
         <Box maw={400} mx="auto">
-            <QuizForm question={demoQuestions} onAnswerSubmit={answers => console.log(answers)}/>
+            <QuizForm question={demoQuestions} onAnswerSubmit={submitAnswer}/>
         </Box>
     );
 }
