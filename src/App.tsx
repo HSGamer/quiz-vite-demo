a63dd6f47e27c23061e607e1606bd247713b7912
HSGamer <huynhqtienvtag@gmail.com>
2023-08-24 13:30:15 +0700

    banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import {demoQuestions, QuestionWithAnswer} from "./question/QuestionList.tsx";
 import {Box} from "@mantine/core";
 import {QuizForm} from "./component/QuizForm.tsx";
+import {useState} from "react";
+import {ImageActionBanner} from "./component/QuizBanner.tsx";
 
 function App() {
     const submitAnswer = (answers: QuestionWithAnswer[]) => {
         console.log(answers)
     };
+    const [started, setStarted] = useState<boolean>();
 
     return (
         <Box maw={400} mx="auto">
-            <QuizForm question={demoQuestions} onAnswerSubmit={submitAnswer}/>
+            {
+                started
+                    ? <QuizForm question={demoQuestions} onAnswerSubmit={submitAnswer}/>
+                    : <ImageActionBanner title={"Demo Quiz"} description={"Start the quiz"} action={{
+                        label: "Start",
+                        action: () => setStarted(true)
+                    }}/>
+            }
         </Box>
     );
 }
